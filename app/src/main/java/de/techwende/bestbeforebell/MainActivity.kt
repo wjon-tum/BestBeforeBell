9b9066c47e1d0abd3b71b5be0b5f241f479c73f1
Jonas Wende <jonas.wende@tum.de>
2025-08-02 14:42:25 +0200

    Initial commit

---
+++ app/src/main/java/de/techwende/bestbeforebell/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.techwende.bestbeforebell
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
